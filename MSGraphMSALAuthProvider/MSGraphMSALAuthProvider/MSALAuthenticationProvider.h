bb0e3014513b69c7de7c8c8cefc40291f4330384
Vikas Dadheech <vidadhee@microsoft.com>
2019-02-18 12:50:33 +0530

    Add pods and ignore contents from checkin in

--- MSGraphMSALAuthProvider/MSGraphMSALAuthProvider/MSALAuthenticationProvider.h
+++ MSGraphMSALAuthProvider/MSGraphMSALAuthProvider/MSALAuthenticationProvider.h
@@ -3,7 +3,7 @@
 //
 #import <Foundation/Foundation.h>
 #import <MSAL/MSAL.h>
-#import <MSGraphClientSDK/MSGraphClientSDK.h>
+#import <MSGraphClientSDK/MSAuthenticationProvider.h>
 
 @interface MSALAuthenticationProvider : NSObject<MSAuthenticationProvider>
 
